f6f434c05658345818be115e97bfdeb7609c0057
TrevSix <98508390+TrevSix@users.noreply.github.com>
2024-01-16 16:56:53 -0700

    Skeleton IDS
    This is post working packet sniffer and ruleset filter. I'm leaving the IDS here and moving on to the SIEM. If time permits, I will return to flesh out the ruleset a bit further.

--- scapy_packet_sniffer.py
+++ scapy_packet_sniffer.py
@@ -1,14 +1,17 @@
 from scapy.all import sniff
 
 def main():
+    ruleset_filter = "tcp and (port 80 or port 22 or port 443)" 
+    user_in = input("Please enter any additional filters you'd like in Berkeley Packet Filter format. \nIncluded by default are TCP ports 80, 443, and 22. \nYour filter here: ")
+    if user_in != "":
+        ruleset_filter = ruleset_filter + ' or ' + user_in
     # Sniffing continuously and writing packets to a file
-    sniff(prn=ruleset_filter, store=0)
-
-def ruleset_filter(packet):
-    if (packet.haslayer("TCP") and packet[TCP].dport == 80):
-        with open ("flagged_packets.txt", "a+") as file:
-            file.write(packet.show(dump=True))
-            file.write("\n\n")
+    print("Filtering by: " + ruleset_filter)
+    try:
+        sniff(prn=packet_printer, filter=ruleset_filter)
+    except PermissionError as e:
+        print("Error Caught in BPF: {e}")
+        print("Please check your filter and try again.")
 
 def packet_printer(packet):
     #print(packet.show())
